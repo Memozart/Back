584c62aa5f9187f675acd5c258d5c82c23ced4cc
Antonin BRINES <antonin.brines@outlook.fr>
2023-02-14 11:22:13 +0100

    fix: 🐛 change to file log in index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const { createLogger } = require('./utils/log');
 
 const port = config.port || 3000;
 const urlBack = config.urlBack || 'http://localhost';
-const logger = createLogger();
+const date = new Date();
+const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+const logger = createLogger(`app/${formattedDate}.log`);
 
 app.listen(port, () => {
   logger.info(`Server running on  ${urlBack}:${port}`);
