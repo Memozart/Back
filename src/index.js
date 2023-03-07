4d350f65ae84f14afb84818545d86b10bb5d372c
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2023-03-07 14:50:50 +0100

    style: 💄 remove library not use + reformat file
    remove library not use + reformat file

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ const urlBack = config.urlBack || 'http://localhost';
 const logger = createLogger();
 
 mongoose.set('strictQuery', false);
-mongoose.connect(`${config.db.url}${config.db.dbName}?authSource=admin&replicaSet=db-mongodb-fra1-85036&tls=true`,()=>{
-  logger.info('Connected to database');
-  app.listen(port, () => {
-    logger.info(`Server running on ${urlBack}:${port}`);
-  });
-});
+mongoose.connect(
+  `${config.db.url}${config.db.dbName}?authSource=admin&replicaSet=db-mongodb-fra1-85036&tls=true`,
+  () => {
+    logger.info('Connected to database');
+    app.listen(port, () => {
+      logger.info(`Server running on ${urlBack}:${port}`);
+    });
+  }
+);
