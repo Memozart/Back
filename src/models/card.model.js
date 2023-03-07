7389e10fe6ca32e40b4fb99e7bae92ae75048b1a
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2023-03-07 17:25:04 +0100

    ajout card service + ajout card model

---
+++ src/models/card.model.js
@@ -0,0 +1,8 @@
+module.exports = class Card {
+  constructor(question, reponse, theme, aide) {
+    this.question = question;
+    this.reponse = reponse;
+    this.theme = theme;
+    this.aide = aide;
+  }
+};
