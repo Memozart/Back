738fc880c8422bbf701bad0f344eb5cbba62581f
ferrere-fabrice <ferrerefabrice@outlook.fr>
2023-03-09 20:28:25 +0100

    docs: ✏️ ajout documentation + corrige erreur message

--- src/services/theme.service.js
+++ src/services/theme.service.js
@@ -1,11 +1,21 @@
 const { Theme } = require('../models');
 const { isEmpty } = require('../utils/tool');
+
+/**
+ * récupère un thème par son id
+ * @param {*} id du thème
+ * @returns retourne le thème ou null si non trouvé
+ */
 const get = async (id) => {
-  if (isEmpty(id)) throw new Error('The parameter must cannot be empty');
+  if (isEmpty(id)) throw new Error('The parameter cannot be empty');
   const theme = Theme.findById(id);
   return theme;
 };
 
+/**
+ * récupère tous les thèmes de la base
+ * @returns retourne tous les thèmes
+ */
 const getAll = async () => {
   const themes = Theme.find();
   return themes;
