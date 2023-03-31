9a063f44c074877ccdb904e897b2e6af573933ed
Antonin BRINES <antonin.brines@outlook.fr>
2023-03-31 15:15:51 +0200

    fix: 🐛 auth token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,15 +12,21 @@ const changeCurrentOrganisation = catchAsync(async (req, res, next) => {
   const { organisationId } = req.body;
   const { userId } = tool.getUserIdAndOrganisationId(req);
   const user = await userService.changeCurrentOrganisation(userId, organisationId);
-  const accessToken = generateAccessToken({ user });
-  const refreshToken = await generateRefreshToken({ user });
-  // add accessToken and refresh token to it
+  const toStore = {
+    _id: user._id,
+    email: user.email,
+    forname: user.forname,
+    lastname: user.lastname,
+    currentOrganisation: user.currentOrganisation._id,
+  };
+  const accessToken = generateAccessToken({ user: toStore });
+  const refreshToken = await generateRefreshToken({ user: toStore });
   const toReturn = {
     auth: {
       refreshToken,
       accessToken
     },
-    organisationId: user.currentOrganisation
+    organisation: user.currentOrganisation,
   };
   successF('Current organisation changed', toReturn, 200, res, next);
 });
