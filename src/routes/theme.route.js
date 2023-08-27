b42ae80f6f4f85a6b033728f776b18c960af6ffe
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2023-08-27 18:26:43 +0200

    fix: 🐛 allow all users get all theme
    suppression du middleware dans la route get all themes

--- src/routes/theme.route.js
+++ src/routes/theme.route.js
@@ -3,6 +3,6 @@ const themeController = require('../controllers/theme.controller');
 
 const userMiddleware = require('../middlewares/user.middleware');
 router.get('/:id', userMiddleware.isConnected, themeController.get);
-router.get('/', userMiddleware.isConnected, themeController.getAll);
+router.get('/', themeController.getAll);
 
 module.exports = router;
