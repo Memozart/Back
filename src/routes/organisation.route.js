bab90bae3d15e488a0145421da2d08585c83dba5
ferrere-fabrice <ferrerefabrice@outlook.fr>
2023-09-07 23:19:16 +0200

    feat: 🎸 récupération utilisateurs dans organisation
    récupère les utilisateurs seulement si admin et présent dans
    l'organisation

--- src/routes/organisation.route.js
+++ src/routes/organisation.route.js
@@ -4,6 +4,7 @@ const userMiddleware = require('../middlewares/user.middleware');
 
 router.get('/:id',userMiddleware.isConnected, organisationController.getById);
 router.get('/', userMiddleware.isConnected, organisationController.getAll);
+router.get('/users/all', userMiddleware.isConnected, organisationController.getAllUserInOrganisation);
 router.post( '/', userMiddleware.isConnected , organisationController.create);
 router.put( '/leave/:id', userMiddleware.isConnected , organisationController.leave);
 router.put( '/join/:id', userMiddleware.isConnected , organisationController.join);
